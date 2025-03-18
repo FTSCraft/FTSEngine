61f52de2bd44137b5e9c9b45b8e479b3af970aee
melle <melleming2001@gmail.com>
2025-03-18 23:51:57 +0100

    CheckPane added

--- src/main/java/de/ftscraft/ftsengine/feature/durchsuchen/DurchsuchenManager.java
+++ src/main/java/de/ftscraft/ftsengine/feature/durchsuchen/DurchsuchenManager.java
@@ -4,6 +4,9 @@ import de.ftscraft.ftsengine.main.Engine;
 import de.ftscraft.ftsengine.utils.ItemStacks;
 import de.ftscraft.ftsengine.utils.Messages;
 import de.ftscraft.ftsutils.items.ItemReader;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
@@ -13,6 +16,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,8 +24,8 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class DurchsuchenManager {
-    private static HashMap <Player, Player> searchRequests = new HashMap<>(); //Player target, Player requester
-    private static HashMap <Player, Integer> searchTasks = new HashMap<>();
+    private static HashMap<Player, Player> searchRequests = new HashMap<>(); //Player target, Player requester
+    private static HashMap<Player, Integer> searchTasks = new HashMap<>();
     private static List<Inventory> searchInventorys = new ArrayList<>();
     private static final int SEARCH_DISTANCE = 5;
     private static Engine plugin = Engine.getInstance();
@@ -32,12 +36,12 @@ public class DurchsuchenManager {
             return;
         }
 
-        if(requesterBlocked(requester)) {
+        if (requesterBlocked(requester)) {
             requester.sendMessage(Messages.PREFIX + "§cDu durchsuchst bereits einen anderen Spieler!");
             return;
         }
 
-        if(!inSearchDistance(target, requester)) {
+        if (!inSearchDistance(target, requester)) {
             requester.sendMessage(Messages.PREFIX + "§cDas Ziel ist zu weit entfernt um durchsucht zu werden!");
             return;
         }
@@ -66,7 +70,7 @@ public class DurchsuchenManager {
 
     public static void accept(Player target) {
         Player requester = searchRequests.get(target);
-        if(!inSearchDistance(target, requester)) {
+        if (!inSearchDistance(target, requester)) {
             requester.sendMessage(Messages.PREFIX + "§cDas Ziel ist zu weit entfernt um durchsucht zu werden!");
             target.sendMessage(Messages.PREFIX + "§cDu bist zu weit entfernt um durchsucht zu werden!");
             return;
@@ -78,17 +82,23 @@ public class DurchsuchenManager {
                 "Inventar von " + target.getName());
         targetInventoryCopy.setContents(target.getInventory().getContents());
 
+        boolean hiddenBundleFailed = false;
         List<Integer> bundles = new ArrayList<>(targetInventoryCopy.all(Material.BUNDLE).keySet());
-        for(int slot : bundles) {
+        for (int slot : bundles) {
             ItemStack stack = targetInventoryCopy.getItem(slot);
-            if(ItemReader.getSign(stack).equals("HIDDEN_BUNDLE")) {
+            if (ItemReader.getSign(stack).equals("HIDDEN_BUNDLE")) {
                 int randValue = ThreadLocalRandom.current().nextInt(0, 100);
-                if(randValue < 90) {
+                if (randValue < 90) {
                     targetInventoryCopy.clear(slot);
+                } else {
+                    hiddenBundleFailed = true;
                 }
             }
         }
 
+        ItemStack checkPane = hiddenBundleFailed ? getCaughtFrame() : getClearFrame();
+
+        targetInventoryCopy.setItem(44, checkPane);
         searchRequests.remove(target);
         Bukkit.getScheduler().cancelTask(searchTasks.get(target));
         searchInventorys.add(targetInventoryCopy);
@@ -98,7 +108,7 @@ public class DurchsuchenManager {
 
     public static void deny(Player target) {
         Player requester = searchRequests.get(target);
-        requester.sendMessage(Messages.PREFIX + "§6" + target.getName() +" §chat die Durchsuchung abgelehnt!");
+        requester.sendMessage(Messages.PREFIX + "§6" + target.getName() + " §chat die Durchsuchung abgelehnt!");
         target.sendMessage(Messages.PREFIX + "§cDu hast die Durchsuchung abgelehnt!");
         searchRequests.remove(target);
         Bukkit.getScheduler().cancelTask(searchTasks.get(target));
@@ -132,6 +142,28 @@ public class DurchsuchenManager {
     private static boolean inSearchDistance(Player target, Player requester) {
         int range = SEARCH_DISTANCE;
         return requester.getLocation().getWorld().getNearbyEntities(requester.getLocation(), range, range, range).contains(target);
+    }
+
+    private static ItemStack getClearFrame() {
+        ItemStack clearFrame = new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1);
+        ItemMeta meta = clearFrame.getItemMeta();
+        String name = "§aDer Spieler scheint nichts zu verstecken!";
+        meta.displayName(Component.text(name));
+        clearFrame.setItemMeta(meta);
+        return clearFrame;
+    }
+
+    private static ItemStack getCaughtFrame() {
+        ItemStack clearFrame = new ItemStack(Material.RED_STAINED_GLASS_PANE, 1);
+        ItemMeta meta = clearFrame.getItemMeta();
+        String name = "§cDer Spieler scheint etwas zu verstecken!";
+        meta.displayName(Component.text(name));
+        String loreText = "§cDu siehst dennoch das gesamte Inventar.";
+        List<Component> lore = new ArrayList<>();
+        lore.add(Component.text(loreText));
+        meta.lore(lore);
+        clearFrame.setItemMeta(meta);
+        return clearFrame;
     }
 
 }
