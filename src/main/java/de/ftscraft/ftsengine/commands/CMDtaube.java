e5b50acad5bf2bbdf0c72dc381dd3721eca4ad22
RxL <devcodedred@gmail.com>
2025-04-13 22:50:44 +0200

    Added Brief as Item to CMDtaube

--- src/main/java/de/ftscraft/ftsengine/commands/CMDtaube.java
+++ src/main/java/de/ftscraft/ftsengine/commands/CMDtaube.java
@@ -3,13 +3,18 @@ package de.ftscraft.ftsengine.commands;
 import de.ftscraft.ftsengine.main.Engine;
 import de.ftscraft.ftsengine.utils.CountdownScheduler;
 import de.ftscraft.ftsengine.utils.Messages;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.jetbrains.annotations.NotNull;
 
 public class CMDtaube implements CommandExecutor {
@@ -31,6 +36,44 @@ public class CMDtaube implements CommandExecutor {
         if (args.length > 1) {
             Player p = (Player) cs;
 
+            // Handle brief command
+            if (args[0].equalsIgnoreCase("brief")) {
+                if (args.length < 3) {
+                    p.sendMessage(Messages.PREFIX + "§cUngültige Verwendung des Befehls!");
+                    return true;
+                }
+
+                String senderName = args[1];
+                StringBuilder message = new StringBuilder();
+                for (int i = 2; i < args.length; i++) {
+                    message.append(" ").append(args[i]);
+                }
+                message = new StringBuilder(message.toString().trim());
+
+                // Check if player has already claimed this brief
+                if (CountdownScheduler.hasClaimedMessage(p, message.toString())) {
+                    p.sendMessage(Messages.PREFIX + "§cDu hast diesen Brief bereits erhalten!");
+                    return true;
+                }
+
+                // Check if player has inventory space
+                if (p.getInventory().firstEmpty() == -1) {
+                    p.sendMessage(Messages.PREFIX + "§cDu brauchst mindestens einen freien Inventarplatz für den Brief!");
+                    return true;
+                }
+
+                ItemStack bookItemStack = new ItemStack(Material.WRITTEN_BOOK);
+                BookMeta bookMeta = (BookMeta) bookItemStack.getItemMeta();
+                bookMeta.displayName(Component.text("Brief von " + senderName).color(NamedTextColor.YELLOW));
+                bookMeta.addPages(Component.text(message.toString()));
+                bookItemStack.setItemMeta(bookMeta);
+
+                p.getInventory().addItem(bookItemStack);
+                p.sendMessage(Messages.PREFIX + "§7Du hast den Brief von der Taube genommen!");
+
+                CountdownScheduler.markMessageAsClaimed(p, message.toString());
+                return true;
+            }
 
             String[] player = args[0].split(",");
 
