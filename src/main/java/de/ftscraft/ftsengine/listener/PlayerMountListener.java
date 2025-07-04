4f3093f6920e11056c01c4ff69eea6853cfb7985
Maaxxs <61059039+MaaxxsDev@users.noreply.github.com>
2025-07-04 11:09:57 +0200

    /gehen command ist jetzt auf pferde anwendbar

--- src/main/java/de/ftscraft/ftsengine/listener/PlayerMountListener.java
+++ src/main/java/de/ftscraft/ftsengine/listener/PlayerMountListener.java
@@ -1,7 +1,9 @@
 package de.ftscraft.ftsengine.listener;
 
+import de.ftscraft.ftsengine.commands.CMDgehen;
 import de.ftscraft.ftsengine.main.Engine;
 import de.ftscraft.ftsengine.utils.Ausweis;
+import de.ftscraft.ftsengine.utils.Messages;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.AbstractHorse;
 import org.bukkit.entity.EntityType;
@@ -9,6 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDismountEvent;
 import org.bukkit.event.entity.EntityMountEvent;
 
 public class PlayerMountListener implements Listener {
@@ -43,5 +46,21 @@ public class PlayerMountListener implements Listener {
         scaleAttribute.setBaseValue(ausweis.getHeight() / 200d);
 
     }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityDismount(EntityDismountEvent event) {
+        EntityType mountType = event.getDismounted().getType();
+        if (mountType != EntityType.HORSE && mountType != EntityType.SKELETON_HORSE && mountType != EntityType.ZOMBIE_HORSE && mountType != EntityType.DONKEY) {
+            return;
+        }
+
+        AbstractHorse horse = (AbstractHorse) event.getDismounted();
+        if(CMDgehen.speed.containsKey(horse)){
+            CMDgehen.setHorseSpeed(horse, CMDgehen.speed.get(horse));
+            CMDgehen.speed.remove(horse);
+            event.getEntity().sendMessage(Messages.PREFIX + " Dein Pferd geht jetzt wieder normal");
+        }
+    }
+
 
 }
